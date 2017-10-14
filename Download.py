09c70c160894d37e914a86447925f5653de44f27
BenGodfrey78 <31974428+BenGodfrey78@users.noreply.github.com>
2017-10-14 18:36:46 +0100

    Download.py
     File

---
+++ Download.py
@@ -0,0 +1,76 @@
+import sys
+import urllib
+
+print (".___                             __________                                  ")
+print ("|   | _____ _____     ____   ____\______   \__ __  ____   ____   ___________ ")
+print ("|   |/     \\__  \   / ___\_/ __ \|       _/  |  \/    \ /    \_/ __ \_  __ |")
+print ("|   |  Y Y  \/ __ \_/ /_/  >  ___/|    |   \  |  /   |  \   |  \  ___/|  | \/")
+print ("|___|__|_|  (____  /\___  / \___  >____|_  /____/|___|  /___|  /\___  >__|   ")
+print ("          \/     \//_____/      \/       \/           \/     \/     \/       ")
+print("")
+print("_____________________________Download_Compleation_Bar_________________________")
+print("")
+
+# setup toolbar
+toolbar_width = 75
+percent = 0
+
+sys.stdout.write("[%s]" % (" " * toolbar_width))
+sys.stdout.flush()
+sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+
+for x in xrange(601)
+	hello == 0	
+	if len(str(hello)) == 1:
+
+		helll = ('00')
+		helll +=str(x)
+		#print helll
+    #Place file extention
+		page = ("http://HERE-HERE_HERE")
+		page+=str(helll)
+		page+=str(".jpg")
+		filee = ("page")
+		filee+=str(helll)
+		filee+=str(".jpg")		
+		urllib.urlretrieve(page, filee)
+		
+		# update the bar
+		#sys.stdout.write("-")
+		#sys.stdout.flush()
+	
+	if len(str(hello)) == 2:
+		two = ('0')
+		two +=str(x)
+		#print hello
+		page = ("HERE HERE HERE")
+		page+=str(two)
+		page+=str(".jpg")
+		filee = ("page")
+		filee+=str(two)
+		filee+=str(".jpg")		
+		urllib.urlretrieve(page, filee)
+		urllib.urlretrieve(page, filee)
+		
+		# update the bar
+		#sys.stdout.write("-")
+		#sys.stdout.flush()
+	
+	if len(str(hello)) == 3:
+		three = ('')
+		three +=str(x)
+		#print three
+		page = ("HERE HERE HERE")
+		page+=str(three)
+		page+=str(".jpg")
+		filee = ("page")
+		filee+=str(three)
+		filee+=str(".jpg")		
+		urllib.urlretrieve(page, filee)
+		
+		# update the bar
+		#sys.stdout.write("-")
+		#sys.stdout.flush()
+sys.stdout.write("\n")s
+
